e8d9e74fe481c2febae2064df001ffbfbc4def6b
John Evans <jjevans1776@gmail.com>
2023-03-26 21:18:17 -0400

    ~

--- methods.py
+++ methods.py
@@ -2,19 +2,19 @@ import os
 
 
 def straight_line():
-  os.system("clear")
-  prCyan("―" * 30)
-  prRed(" * " * 10)
-  prCyan("―" * 30)
+    os.system("clear")
+    prCyan("―" * 30)
+    prRed(" * " * 10)
+    prCyan("―" * 30)
 
 
 def press_enter():
-  input("\n*** Press Enter ***\n")
+    input("\n*** Press Enter ***")
 
 
 def prCyan(skk):
-  print("\033[96m{}\033[00m".format(skk))
+    print("\033[96m{}\033[00m".format(skk))
 
 
 def prRed(skk):
-  print("\033[91m{}\033[00m".format(skk))
+    print("\033[91m{}\033[00m".format(skk))
