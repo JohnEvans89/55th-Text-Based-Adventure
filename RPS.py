e95e456b7da93645e517fcdc87721ca2f8da15a2
John Evans <60052305+JohnEvans89@users.noreply.github.com>
2023-03-31 12:57:02 +0000

    RPS-moved the art from RPS to ascii. refactored the computer choice method

--- RPS.py
+++ RPS.py
@@ -1,38 +1,26 @@
 import random
+import ascii as art
 # RPS game for the ATC "enemy"
-scissors_ascii = '''
-    _       ,/'
-   (_).  ,/'
-   __  ::
-  (__)'  `\.
-            `\.'''
-
-paper_ascii = '''
-    _______
-   /      /,
-  /      //
- /______//
-(______(/'''
-
-rock_ascii = '''
- _____
-| | | |/\\
-|_|_|_|\ \\
-|        /
-\_______/'''
-
 def RPS_battle(comp,user):
   print(comp,"\n\t~~\n\tVS\n\t~~\n", user)
 
 
-mylist = [rock_ascii,paper_ascii,scissors_ascii]
+mylist = [art.rock_ascii,art.paper_ascii,art.scissors_ascii]
 
-# comp_choice=random.choice(mylist)
-# print(comp_choice)
-# if comp_choice==rock_ascii:
-#   print("Rock")
-# elif comp_choice==paper_ascii:
-#   print("Paper")
-# elif comp_choice==scissors_ascii:
-#   print("Scissors")
-RPS_battle(rock_ascii,paper_ascii)
+def computer_player():
+  comp_choice=random.choice(mylist)
+  print(comp_choice)
+  if comp_choice==art.rock_ascii:
+    print("Rock")
+  elif comp_choice==art.paper_ascii:
+    print("Paper")
+  elif comp_choice==art.scissors_ascii:
+    print("Scissors")
+  else:
+    print("Something broke!!!")
+# RPS_battle(art.rock_ascii,art.scissors_asci)
+p=0
+while p<5:
+  computer_player()
+  p+=1
+  
