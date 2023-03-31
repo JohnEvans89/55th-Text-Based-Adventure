b3e9f916b59203485b967a59ee1e94e663e6215a
John Evans <60052305+JohnEvans89@users.noreply.github.com>
2023-03-31 14:20:28 +0000

    RPS_battle had a bug. It was printing the choice art and type, but when it got to the battle it returned 'none'. I had to add a return statement to computer_player and remove the test print statement

--- RPS.py
+++ RPS.py
@@ -1,26 +1,28 @@
 import random
 import ascii as art
+
+RPS_art = [art.rock_ascii, art.paper_ascii, art.scissors_ascii]
+
+
 # RPS game for the ATC "enemy"
-def RPS_battle(comp,user):
-  print(comp,"\n\t~~\n\tVS\n\t~~\n", user)
+def RPS_battle(comp, user):
+  print(comp, "\n\t~~\n\tVS\n\t~~\n", user)
 
-
-mylist = [art.rock_ascii,art.paper_ascii,art.scissors_ascii]
 
 def computer_player():
-  comp_choice=random.choice(mylist)
-  print(comp_choice)
-  if comp_choice==art.rock_ascii:
-    print("Rock")
-  elif comp_choice==art.paper_ascii:
+  comp_choice = random.choice(RPS_art)
+  # print(comp_choice)
+  if comp_choice == art.rock_ascii:
+    comp_choice="Rock\n"+comp_choice
+  elif comp_choice == art.paper_ascii:
     print("Paper")
-  elif comp_choice==art.scissors_ascii:
+  elif comp_choice == art.scissors_ascii:
     print("Scissors")
   else:
     print("Something broke!!!")
+  return comp_choice
+
+
 # RPS_battle(art.rock_ascii,art.scissors_asci)
-p=0
-while p<5:
-  computer_player()
-  p+=1
-  
+
+RPS_battle(computer_player(),computer_player())
