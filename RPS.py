3543de9e66e245453d93e9996a479cdb554bf230
John Evans <60052305+JohnEvans89@users.noreply.github.com>
2023-04-03 18:12:55 +0000

    RPS-refactored RPS_player_choice and battle to return art rather than converting from rock --> r --> art.rock

--- RPS.py
+++ RPS.py
@@ -7,33 +7,22 @@ RPS_art = [art.rock_ascii_2, art.paper_ascii_2, art.scissors_ascii_2]
 def RPS_player_choice():
   player_choice = input(
     "Make Your Choice: \n\t1. Rock\n\t2. Paper\n\t3. Scissors\n--->")
-  if player_choice == '1':
-    return 'r'
-  elif player_choice == '2':
-    return 'p'
-  else:
-    return 's'
+  if player_choice == '1': return art.rock_ascii_2
+  elif player_choice == '2': return art.paper_ascii_2
+  else: return art.scissors_ascii_2
 
 
 # RPS game for the ATC "enemy"
-def RPS_battle(user):
+def RPS_battle():
+  user=RPS_player_choice()
+  win_lose=user
   user_choice = ''
   comp_choice = random.choice(RPS_art)
-  if user == 'r':
-    user_choice = art.rock_ascii_2
-  elif user == 'p':
-    user_choice = art.paper_ascii_2
-  else:
-    user_choice = art.scissors_ascii_2
-    
+  if user == 'r': user_choice = art.rock_ascii_2
+  elif user == 'p': user_choice = art.paper_ascii_2
+  else: user_choice = art.scissors_ascii_2
   print(comp_choice, "\n\t~~\n\tVS\n\t~~\n", user_choice)
-
-
-
-# RPS_battle(art.rock_ascii,art.scissors_asci)
-
-
-
+  # if win_lose=='r' and 
 def ATC_53():
   print(art.ATC_ascii)
   print(
@@ -44,9 +33,8 @@ def ATC_53():
 # print(art.ATC_ascii,art.paper_ascii_2)
 # ATC_53()
 # print(random.choice(RPS_art))
-n=5
-while n>0:
+n = 5
+while n > 0:
   print(n)
-  te=RPS_player_choice()
-  RPS_battle(te)
-  n-=1
+  print(RPS_player_choice())
+  n -= 1
