81cea0e40e560c8d120fa4dd6063b47d23bb9855
John Evans <60052305+JohnEvans89@users.noreply.github.com>
2023-04-04 20:18:59 +0000

    RPS-added win/lose counter and return statement if win or lose is above 3

--- RPS.py
+++ RPS.py
@@ -1,7 +1,7 @@
 import random
 import ascii as art
 import os
-RPS_art = [art.rock_ascii_2, art.paper_ascii_2, art.scissors_ascii_2]
+RPS_art = [art.rock_ascii_2]
 
 
 def RPS_player_choice():
@@ -14,16 +14,29 @@ def RPS_player_choice():
 
 # RPS game for the ATC "enemy"
 def RPS_battle():
-  user = RPS_player_choice()
-  comp_choice = random.choice(RPS_art)
-  print(user, "\n\t~~\n\tVS\n\t~~\n", comp_choice)
-  if user == comp_choice:
-    print("Draw")
-  elif (user==art.rock_ascii_2 and comp_choice==art.scissors_ascii_2) or (user==art.scissors_ascii_2 and comp_choice==art.paper_ascii_2) or (user==art.paper_ascii_2 and comp_choice==art.rock_ascii_2):
-    print("you win")
-  else:
-    print("lose")
-  
+  win=0
+  lose=0
+  while True:
+    print(f"Wins: {win} Loses: {lose}")
+    user = RPS_player_choice()
+    comp_choice = random.choice(RPS_art)
+    print(user, "\n\t~~\n\tVS\n\t~~\n", comp_choice)
+    if user == comp_choice:
+      print("Draw")
+    elif (user==art.rock_ascii_2 and comp_choice==art.scissors_ascii_2) or (user==art.scissors_ascii_2 and comp_choice==art.paper_ascii_2) or (user==art.paper_ascii_2 and comp_choice==art.rock_ascii_2):
+      print("you win")
+      win +=1
+      if win>=3:
+        # break
+        return "w"
+    else:
+      print("lose")
+      lose +=1
+      if lose>=3:
+        # break
+        return "l"
+
+
 
 
 def ATC_53():
@@ -36,9 +49,6 @@ def ATC_53():
 # print(art.ATC_ascii,art.paper_ascii_2)
 # ATC_53()
 # print(random.choice(RPS_art))
-n = 5
-while n > 0:
-  os.system("clear")
-  RPS_battle()
-  n -= 1
-  enter=input('')
+
+
+RPS_battle()
